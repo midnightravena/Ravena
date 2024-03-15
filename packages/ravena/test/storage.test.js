5bc7db549d500c78f7db540ff9a61a5f1aa4b43e
Sahil <151955025+sahilarun@users.noreply.github.com>
2024-03-15 15:50:58 +0530

    Create storage.test.js

---
+++ packages/ravena/test/storage.test.js
@@ -0,0 +1,49 @@
+localStorage.bar = "baz";
+jest.resetModules();
+
+const m = require("ravena/src/index").default.m;
+
+test("sets storage initially", () => {
+	m.storage.foo = "bar";
+	m.storage.moon = "titan";
+	expect(localStorage).toEqual(m.storage);
+});
+
+test("updates storage as needed", () => {
+	m.storage.foo = "bar";
+	m.storage.moon = "titan";
+	expect(localStorage).toEqual(m.storage);
+
+	m.storage.foo = "bar";
+	m.storage.moon = "europa";
+	expect(localStorage).toEqual(m.storage);
+});
+
+test("removes storage as needed", () => {
+	delete m.storage.foo;
+	expect(localStorage).toEqual(m.storage);
+});
+
+test("sets whole storage", () => {
+	m.storage = {};
+	expect(localStorage).toEqual(m.storage);
+
+	m.storage = {
+		foo: "bar",
+		moon: "titan"
+	};
+	expect(localStorage).toEqual(m.storage);
+});
+
+test("updates whole storage", () => {
+	m.storage = {
+		moon: "titan",
+		bar: "baz"
+	};
+	expect(localStorage).toEqual(m.storage);
+});
+
+test("deletes whole storage", () => {
+	m.storage = {};
+	expect(localStorage).toEqual(m.storage);
+});
